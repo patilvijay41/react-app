0f531a78f119fa0eaa7ac71c34a9fda5d8106227
Vijay <patilvijay41@gmail.com>
2020-12-22 11:32:29 +0530

    prod heading

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React application for development
+          Learn React application for development and production
         </a>
       </header>
     </div>
